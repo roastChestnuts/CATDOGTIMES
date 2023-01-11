303d290d641eabe2d6024638a28d8e5e71c1086f
정재열(RE-Heat) <whitewolf1540@gmail.com>
2023-01-12 01:15:57 +0900

    주요작업 : PostController 작성, post-mapper.xml update·delete 수정
    [1] PostController.java
      SNS 게시글 관련 컨트롤러
      ① SNS 게시글 작성 <== 현재 리액트 연동해서 구현하고 확인된 건 이것 뿐.
      ② SNS 전체조회 <== 아직 테스트 안 해봄
      ③ SNS 게시글 수정 <== 아직 테스트 안 해봄
      ④ SNS 게시글 삭제  <== db에서 삭제는 되는데, 에러 뜸. 향후 확인 필요
    
    [2] post-mapper.xml
      1] keyProperty(반환값을 매핑할 변수명)=no에서 각 keyColumn에 맞는 값으로 교체함.
      (insert는 나중에 필요없으면 id만 남기고 삭제할 의향 있음)
      2] update·delete
         update·delete <select>로 쓰는 실수를 저질러 전부 수정,
         parameterType은 map으로 교체했는데 이 부분도 공부 필요
    
    [3] hellocontroller.java
       리액트<>스프링 연동하는 과정에서 만든 임시파일. 익숙해지면 조만간 삭제 예정

--- CATDOGTIMES/src/main/java/com/catdog/times/post/controller/PostController.java
+++ CATDOGTIMES/src/main/java/com/catdog/times/post/controller/PostController.java
@@ -1,15 +1,63 @@
 package com.catdog.times.post.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.catdog.times.post.model.dto.PostDTO;
 import com.catdog.times.post.model.service.PostService;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Controller
+@RestController
+@RequestMapping("/post")
 public class PostController {
 	@Autowired
 	private PostService service;
+
+	// SNS 게시글 작성
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String insertPost(@RequestBody PostDTO post) {
+		System.out.println(post);
+		int result = service.insertPost(post);
+		System.out.println("insert 결과값:" + result);
+		System.out.println("return값:" + post.getPostId());
+		return "성공";
+	}
+
+	// SNS 게시글(Post) 전체조회
+
+	@RequestMapping("/list")
+	public ModelAndView selectAllpost() {
+		ModelAndView mav = new ModelAndView("list");
+		List<PostDTO> postlist = service.selectAllPost();
+		mav.addObject("postlist", postlist);
+		return mav;
+	}
+
+	// SNS 게시글 수정
+	public ModelAndView postUpdate(PostDTO post) {
+		ModelAndView mav = new ModelAndView();
+		int result = service.postUpdate(post);
+		if (result > 0) {
+			mav.setViewName("redirect:/list");
+		} else {
+			mav.setViewName(null); // redirect 어디로?
+		}
+		mav.addObject("post", post);
+		return mav;
+	}
+	// SNS 게시글 삭제 로그인 유무 체크 필요한가...
+
+	@RequestMapping(value = "/delete")
+	public String deletePost(String postId) {
+		System.out.println(postId);
+		// 로그인 사용자 찾기 (우선 pass)
+		int result = service.deletePost(Integer.parseInt(postId));		
+		return "성공";
+	}
 }
